50bc69dfb5af775cc792b03d052ad636dd115ce4
yadaram srikanth <yadaramsrikanth2000@gmail.com>
2024-07-30 14:26:38 +0530

    vsnodelogin

---
+++ app.js
@@ -0,0 +1,53 @@
+const express=require("express")
+const app=express()
+app.use(express.json())
+const sqlite3=require("sqlite3")
+const {open} =require("sqlite")
+const path=require("path")
+const { request } = require("http")
+
+const dbPath=path.join(__dirname,"user.db")
+let db=null
+const initializeDBAndServer=async ()=>{
+       try{
+
+        db=await open({
+            filename:dbPath,
+            driver:sqlite3.Database
+        })
+        app.listen(3005,()=>{
+            console.log("port running")
+        })
+        
+       }catch(e){
+        console.log(`DB Error: ${e.message}`)
+        process.exit(1)
+       }
+       
+
+}
+
+initializeDBAndServer()
+
+app.get("/",async(request,response)=>{
+    const selectQuery=`select * from user`
+    const responsedata=await db.all(selectQuery)
+    response.send(responsedata)
+    console.log(responsedata)
+})
+
+app.post("/login",async(request,response)=>{
+    const {username,password}=request.body
+    const selectQuery=`select * from user where username='${username}';`;
+    const responsedata=await db.get(selectQuery)
+    console.log(responsedata)
+    if(responsedata===undefined){
+        const createQuery=`insert into user (username,password) values ('${username}','${password}');`;
+
+        await db.run(createQuery)
+        response.send("user created succeefully")
+    }else{
+        response.send("user already exists")
+    }
+})
+
