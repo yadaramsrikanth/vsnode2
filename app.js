d41bba1fe02ac9e6fd8a8240fe19730baf849175
yadaram srikanth <yadaramsrikanth2000@gmail.com>
2024-08-06 20:12:55 +0530

    removed localhost

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path=require("path")
 
 const cors=require("cors")
 
-app.use(cors({origin:'http://localhost:3004'}))
+app.use(cors({origin:'*'}))
 
 const dbPath=path.join(__dirname,"user.db")
 let db=null
